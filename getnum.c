f24614a8b3ae5af8901dfde2fce2d189ad7e5173
mdedeu <62359173+mdedeu@users.noreply.github.com>
2020-05-09 17:08:09 -0300

    Update getnum.c

--- getnum.c
+++ getnum.c
@@ -1,110 +1,109 @@
 #include <stdio.h>
 #include "getnum.h"
 #include <string.h>
+#define CLEAN_BUFFER while (getchar() != '\n')
 
-#define BORRA_BUFFER while (getchar() != '\n')
-
-/* lee un entero */
+/* reads an integer */
 int 
-getint(const char mensaje[], ... )
+getint(const char msg[], ... )
 {
-	int n, salir = 0;
+	int n, done = 0;
 	va_list ap;
 		
 	do	
 	{
-		va_start(ap, mensaje);
-		vprintf(mensaje, ap);
+		va_start(ap, msg);
+		vprintf(msg, ap);
 		va_end(ap);
 
 		if ( scanf("%d",&n) != 1)
 		{
-			printf("\nDato incorrecto\n");
-			BORRA_BUFFER;
+			printf("\nIncorrect entry\n");
+			CLEAN_BUFFER;
 		}
 		else
-			salir = 1;
-	} while (! salir);
-	BORRA_BUFFER;
+			done = 1;
+	} while (! done);
+	CLEAN_BUFFER;
 	return n;
 }
 
-/* lee un float */
+/* reads a float */
 float 
-getfloat(const char mensaje[], ...)
+getfloat(const char msg[], ...)
 {
 	float n;
-	int salir = 0;
+	int done = 0;
 	va_list ap;
 		
 	do	
 	{
-		va_start(ap, mensaje);
-		vprintf(mensaje, ap);
+		va_start(ap, msg);
+		vprintf(msg, ap);
 		va_end(ap);
 
 		if ( scanf("%g",&n) != 1)
 		{
-			printf("\nDato incorrecto\n");
-			BORRA_BUFFER;
+			printf("\nIncorrect Entry\n");
+			CLEAN_BUFFER;
 		}
 		else
-			salir = 1;
-	} while (! salir);
-	BORRA_BUFFER;
+			done = 1;
+	} while (! done);
+	CLEAN_BUFFER;
 	return n;
 }
 
-/* lee un double */
+/*reads a double */
 double 
-getdouble(const char mensaje[], ...)
+getdouble(const char msg[], ...)
 {
 	double n;
-	int salir = 0;
+	int done = 0;
 	va_list ap;
 		
 	do	
 	{
-		va_start(ap, mensaje);
-		vprintf(mensaje, ap);
+		va_start(ap, msg);
+		vprintf(msg, ap);
 		va_end(ap);
 
 		if ( scanf("%lg",&n) != 1)
 		{
-			printf("\nDato incorrecto\n");
-			BORRA_BUFFER;
+			printf("\nIncorrect Entry\n");
+			CLEAN_BUFFER;
 		}
 		else
-			salir = 1;
-	} while (! salir);
-	BORRA_BUFFER;
+			done = 1;
+	} while (! done);
+	CLEAN_BUFFER;
 	return n;
 }
 
 /* lee S,s,N, o n  */
 int 
-yesNo(const char mensaje[], ...)
+yesNo(const char msg[], ...)
 {
 	int op;
-	int salir = 0;
+	int done = 0;
 	va_list ap;
 		
 	do	
 	{
-		va_start(ap, mensaje);
-		vprintf(mensaje, ap);
+		va_start(ap, msg);
+		vprintf(msg, ap);
 		va_end(ap);
 
 		op = getchar();
 		if ( strchr("SsNn", op) == NULL)
 		{
-			printf("\nDato incorrecto\n");
+			printf("\nIncorrect Entry\n");
 			if (op != '\n')
-				BORRA_BUFFER;
+				CLEAN_BUFFER;
 		}
 		else
-			salir = 1;
-	} while (! salir);
-	BORRA_BUFFER;
+			done = 1;
+	} while (! done);
+	CLEAN_BUFFER;
 	return strchr("Ss", op) != NULL;
 }
